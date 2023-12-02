996f3981983358d320227346823793b9d7708ca4
jadakiki1 <79327461+jadakiki1@users.noreply.github.com>
2023-12-02 15:21:23 +0200

    Update AggregatedDataDto.cs

--- ParserFromTxtToCsv/AggregatedDataDto.cs
+++ ParserFromTxtToCsv/AggregatedDataDto.cs
@@ -1,11 +1,11 @@
-﻿using System.Data.Odbc;
+using System.Data.Odbc;
 using Vertica.Data.Internal.DotNetDSI;
 
 namespace BabyNi
 {
     public class AggregatedDataDto
     {
-        public DateTime Time { get; set; }
+        public DateTime DateTime_Key { get; set; }
         public string NeAlias { get; set; }
         public string NeType { get; set; }
         public double RFInputPower { get; set; }
@@ -23,7 +23,7 @@ namespace BabyNi
             using (OdbcConnection conn = new OdbcConnection("Driver={Vertica};Server=10.10.4.231;Database=test;User=bootcamp7;Password=bootcamp72023;"))
             {
                 conn.Open();
-                string query = $"SELECT Time, NeAlias, NeType, RFInputPower, MaxRxLevel, RSL_Deviation FROM {tableName}";
+                string query = $"SELECT DateTime_Key, NeAlias, NeType, RFInputPower, MaxRxLevel, RSL_Deviation FROM {tableName}";
                 using (OdbcCommand cmd = new OdbcCommand(query, conn))
                 using (OdbcDataReader reader = cmd.ExecuteReader())
                 {
@@ -31,7 +31,7 @@ namespace BabyNi
                     {
                         var data = new AggregatedDataDto
                         {
-                            Time = reader.GetDateTime(reader.GetOrdinal("Time")),
+                            DateTime_Key = reader.GetDateTime(reader.GetOrdinal("DateTime_Key")),
                             NeAlias = reader.GetString(reader.GetOrdinal("NeAlias")),
                             NeType = reader.GetString(reader.GetOrdinal("NeType")),
                             RFInputPower = reader.GetDouble(reader.GetOrdinal("RFInputPower")),
